00b13eb6ca501e6b6a3a1e6c27a965ab371e5ba6
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-11-07 18:15:20 +0500

    division 3

---
+++ arithmetic_op05.py
@@ -0,0 +1,9 @@
+#Create a variable called 'number' with data type int.
+ 
+#Create a variable called 'answer'
+#Find the remainder of the division of number by 3.
+ 
+#Print the answer
+number=10
+answer=number/3
+print(answer)
